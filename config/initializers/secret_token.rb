0922f6e97fdcb4a917bdee53bb1d87377521a98d
1018512841 <1018512841@qq.com>
2014-05-06 22:13:24 +0800

    first commit a empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FansHome::Application.config.secret_key_base = '5cd6b8c1b1f6835dff478d494b484fc81ad2a4b719fe2f810d05bd093d52e88be981e864c8de33a6836abd04fdc131dd67746c55ab6f1818ad951c41a13b4624'
