6f73518532f9b44817654cfb8efd0a1661e543f4
1018512841 <1018512841@qq.com>
2015-03-08 18:16:32 +0800

    修改helper方法

--- app/helpers/static_page_helper.rb
+++ app/helpers/static_page_helper.rb
@@ -1,18 +1,11 @@
 module StaticPageHelper
   def generate_navbar_item(url, text)
-    html_text = <<EOS
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#{url}">
-      #{text}</a></li>
-EOS
-    return html_text
-
+    content_tag 'li', role: 'presentation' do
+      link_to text, url, role: 'menuitem', tabindex: '-1'
+    end
   end
 
   def generate_tooltip_link(url, tooltip_text, class_name)
-    html_text = <<EOS
-    <a href="#{url}" class="#{class_name}" rel="tooltip"
-data-placement="top" data-original-title=#{tooltip_text}></a>
-EOS
-    return html_text
+    link_to '', url, class: class_name, rel: 'tooltip', data: {placement: 'top', original_title: tooltip_text}
   end
 end
